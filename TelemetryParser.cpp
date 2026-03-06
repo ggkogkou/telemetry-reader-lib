dab2bfbceb6c1a49d244d4fa038a3fea36b25d14
Georgios Gkogkou <georgios.gkogkou@cern.ch>
2026-03-06 14:55:36 +0100

    Implement the readFrame() function

--- TelemetryParser.cpp
+++ TelemetryParser.cpp
@@ -8,6 +8,114 @@ RadiationTestTelemetry::TelemetryParser::TelemetryParser(boost::asio::io_context
         serialPort.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
         serialPort.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
         serialPort.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
+
+        buffer.reserve(4096);
 }
 
+void RadiationTestTelemetry::TelemetryParser::fillBuffer(std::size_t minBytes) {
+        static constexpr std::size_t MaximumBytesSequence = 512;
 
+        const std::size_t ReadSize = std::max<std::size_t>(minBytes, MaximumBytesSequence);
+
+        std::vector<std::uint8_t> tmpBuffer(ReadSize);
+
+        const std::size_t BytesRead = serialPort.read_some(boost::asio::buffer(tmpBuffer));
+
+        if (BytesRead > 0)
+                buffer.insert(buffer.end(), tmpBuffer.begin(), tmpBuffer.begin() + static_cast<std::ptrdiff_t>(BytesRead));
+}
+
+std::uint16_t RadiationTestTelemetry::TelemetryParser::crc16_CCITT(const std::span<std::uint8_t> data, std::uint16_t crc) {
+        const auto DataSize = data.size();
+
+        for (std::size_t i = 0; i < DataSize; i++) {
+                crc ^= data[i] << 8;
+
+                for (int bit = 0; bit < 8; bit++) {
+                        if ((crc & 0x8000U) != 0U)
+                                crc = static_cast<std::uint16_t>((crc << 1) ^ 0x1021U);
+                        else
+                                crc = static_cast<std::uint16_t>(crc << 1);
+                }
+        }
+
+        return crc;
+}
+
+std::optional<std::vector<std::uint8_t>> RadiationTestTelemetry::TelemetryParser::readFrame(seconds_t timeoutSeconds) {
+        using namespace RadiationTestTelemetry::PayloadDecodingConstants;
+
+        const auto Deadline = std::chrono::steady_clock::now() + std::chrono::duration<float>(timeoutSeconds);
+
+        while (std::chrono::steady_clock::now() < Deadline) {
+                if (buffer.size() < 4) {
+                        fillBuffer(4 - buffer.size());
+                        continue;
+                }
+
+                auto syncIterator = std::search(buffer.begin(), buffer.end(), std::begin(std::array{SYNC_BYTE_0, SYNC_BYTE_1}),
+                                                std::end(std::array{SYNC_BYTE_0, SYNC_BYTE_1}));
+
+                if (syncIterator == buffer.end()) {
+                        if (not buffer.empty()) {
+                                const std::uint8_t LastByte = buffer.back();
+                                buffer.clear();
+                                buffer.push_back(LastByte);
+                        }
+                        fillBuffer();
+                        continue;
+                }
+
+                const auto SyncIdx = static_cast<std::size_t>(std::distance(buffer.begin(), syncIterator));
+
+                if (SyncIdx > 0)
+                        buffer.erase(buffer.begin(), buffer.begin() + static_cast<std::ptrdiff_t>(SyncIdx));
+
+                if (buffer.size() < 4) {
+                        fillBuffer(4 - buffer.size());
+                        continue;
+                }
+
+                const std::uint8_t Length = buffer[2];
+
+                if (const std::uint8_t MessageType = buffer[3]; MessageType != TYPE_TELEMETRY) {
+                        buffer.erase(buffer.begin());
+                        continue;
+                }
+
+                const std::size_t FrameSize = 2 + 1 + static_cast<std::size_t>(Length) + 2;
+
+                if (buffer.size() < FrameSize) {
+                        fillBuffer(FrameSize - buffer.size());
+                        continue;
+                }
+
+                std::vector frame(buffer.begin(), buffer.begin() + static_cast<std::ptrdiff_t>(FrameSize));
+
+                buffer.erase(buffer.begin(), buffer.begin() + static_cast<std::ptrdiff_t>(FrameSize));
+
+                std::vector crcData(frame.begin() + 3, frame.begin() + 3 + Length);
+
+                const auto CRC_Calc = crc16_CCITT(crcData);
+                const auto CRC_ReceivedLo = static_cast<std::uint16_t>(frame[FrameSize-2]);
+                const auto CRC_ReceivedHi = static_cast<std::uint16_t>(frame[FrameSize-1] << 8);
+                const auto CRC_Received = static_cast<std::uint16_t>(CRC_ReceivedLo | CRC_ReceivedHi);
+
+                if (CRC_Received != CRC_Calc) {
+                        if (not buffer.empty()) {
+                                const std::uint8_t LastByte = buffer.back();
+                                buffer.clear();
+                                buffer.push_back(LastByte);
+                        }
+                        continue;
+                }
+
+                return frame;
+        }
+
+        return std::nullopt;
+}
+
+bool RadiationTestTelemetry::TelemetryParser::decodePayload44(std::span<std::uint8_t> payload, Telemetry44& parametersMonitor) {
+
+}
--- TelemetryParser.hpp
+++ TelemetryParser.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <algorithm>
+#include <array>
 #include <boost/asio.hpp>
+#include <chrono>
 #include <cstddef>
 #include <cstdint>
+#include <optional>
+#include <span>
 #include <string>
 #include <vector>
 
@@ -19,10 +24,12 @@ namespace RadiationTestTelemetry {
  * All the necessary constants that are used to decode the received encoder payload
  */
 namespace PayloadDecodingConstants {
+
 inline constexpr std::uint8_t SYNC_BYTE_0 = 0xA5;
 inline constexpr std::uint8_t SYNC_BYTE_1 = 0x5A;
 inline constexpr std::uint8_t TYPE_TELEMETRY = 0x01;
 inline constexpr std::size_t PAYLOAD_44_SIZE = 44;
+
 } // namespace PayloadDecodingConstants
 
 /**
@@ -43,8 +50,6 @@ struct Telemetry44 {
         std::int32_t omega_mrad_s = 0;
         std::uint32_t encoder_error_code = 0;
 };
-
-static_assert(sizeof(Telemetry44) == PayloadDecodingConstants::PAYLOAD_44_SIZE, "The Telemetry44 struct is different than 44 bytes");
 
 /**
  * @class TelemetryParser
@@ -74,6 +79,51 @@ private:
          * A buffer for storing the received data
          */
         std::vector<std::uint8_t> buffer{};
+
+        using seconds_t = float;
+
+        /**
+         * Function that keeps filling the byte buffer till enough data exists
+         *
+         * Logic behind function:
+         *
+         * - Until timeout, if less than 4 bytes were buffered, fill more. They should be more than the necessary SUNC0, SYNC1, LEN,
+         *      TYPE to be able to be decoded
+         * - Search the buffer for SYNC0|SYNC1 for the re-synchronization
+         * - Once sync is aligned at buffer[0], inspect len = buffer[2] and msgType = buffer[3] and discard byte if necessary
+         * - Compute full frame size: SYNC0 + SYNC1 + LEN + length{type+payload} + CRC_HI + CRC_LO
+         * - Validate the aforementioned data; check if they make sense
+         * - Compute CRC over msgType + payload, and compare to received CRC
+         *      - If CRC fails -> keep only last byte of buffer and continue
+         *      - If CRC passes -> return the frame
+         *
+         * @param timeoutSeconds A timeout for the receiving data
+         * @return
+         */
+        std::optional<std::vector<std::uint8_t>> readFrame(seconds_t timeoutSeconds = 2.0f);
+
+        /**
+         * Function that decoded the payload that is given to it as argument
+         *
+         * @param payload The telemetry payload (e.g. 44 bytes)
+         * @param parametersMonitor
+         * @return
+         */
+        bool decodePayload44(std::span<std::uint8_t> payload, Telemetry44& parametersMonitor);
+
+        /**
+         * Function that reads some bytes from serial and appends them to the internal buffer
+         */
+        void fillBuffer(std::size_t minBytes = 1);
+
+        /**
+         * Function that computes a CRC-16-CCITT checksum over a byte sequence
+         *
+         * @param data An std::span containing the bytes sequence
+         * @param crc
+         * @return
+         */
+        static std::uint16_t crc16_CCITT(std::span<std::uint8_t> data, std::uint16_t crc = 0xFFFF);
 };
 
 } // namespace RadiationTestTelemetry
